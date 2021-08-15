2d28c09dff616a28551a56c7900d1d23dcc70b57
Gardar Hauksson <gardarh@gmail.com>
2021-08-15 08:45:03 +0000

    Suppress output in daily script.

--- make_thumbs_and_db.sh
+++ make_thumbs_and_db.sh
@@ -4,5 +4,4 @@ cd $SCRIPT_DIR
 . ./.env.local
 . env/bin/activate
 cd django_homealbum
-# python manage.py makethumbs > /dev/null 2>&1 && python manage.py makedb > /dev/null 2>&1
-python manage.py makethumbs && python manage.py makedb
+python manage.py makethumbs > /dev/null 2>&1 && python manage.py makedb > /dev/null 2>&1
